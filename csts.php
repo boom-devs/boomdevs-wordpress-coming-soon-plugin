873667c46c33690b8a6b4285651194b2293e30d7
EnamulHaq <mdenamulhaq6263@gmail.com>
2020-11-23 13:07:27 +0600

    Update csts.php

--- csts.php
+++ csts.php
@@ -14,7 +14,7 @@
  *
  * @wordpress-plugin
  * Plugin Name:       TS Wordpress Coming Soon Template 
- * Plugin URI:        taspristudio.com/dev/cst
+ * Plugin URI:        taspristudio.com
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
  * Author:            taspristudio
