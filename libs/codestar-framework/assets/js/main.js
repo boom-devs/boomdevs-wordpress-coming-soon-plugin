49d27dea3b98a110cd31c4e1c32d03b469d52c04
noruzzamanrubel <noruzzamanrubel@gmail.com>
2024-01-29 18:22:08 +0600

    update codestar version

--- libs/codestar-framework/assets/js/main.js
+++ libs/codestar-framework/assets/js/main.js
@@ -245,7 +245,7 @@
   $.fn.csf_page_templates = function() {
     if ( this.length ) {
 
-      $(document).on('change', '.editor-page-attributes__template select, #page_template', function() {
+      $(document).on('change', '.editor-page-attributes__template select, #page_template, .edit-post-post-status + div select', function() {
 
         var maybe_value = $(this).val() || 'default';
 
@@ -814,17 +814,19 @@
   $.fn.csf_field_group = function() {
     return this.each( function() {
 
-      var $this     = $(this),
-          $fieldset = $this.children('.csf-fieldset'),
-          $group    = $fieldset.length ? $fieldset : $this,
-          $wrapper  = $group.children('.csf-cloneable-wrapper'),
-          $hidden   = $group.children('.csf-cloneable-hidden'),
-          $max      = $group.children('.csf-cloneable-max'),
-          $min      = $group.children('.csf-cloneable-min'),
-          field_id  = $wrapper.data('field-id'),
-          is_number = Boolean( Number( $wrapper.data('title-number') ) ),
-          max       = parseInt( $wrapper.data('max') ),
-          min       = parseInt( $wrapper.data('min') );
+      var $this           = $(this),
+          $fieldset    = $this.children('.csf-fieldset'),
+          $group       = $fieldset.length ? $fieldset : $this,
+          $wrapper     = $group.children('.csf-cloneable-wrapper'),
+          $hidden      = $group.children('.csf-cloneable-hidden'),
+          $max         = $group.children('.csf-cloneable-max'),
+          $min         = $group.children('.csf-cloneable-min'),
+          title_by     = $wrapper.data('title-by'),
+          title_prefix = $wrapper.data('title-by-prefix'),
+          field_id     = $wrapper.data('field-id'),
+          is_number    = Boolean( Number( $wrapper.data('title-number') ) ),
+          max          = parseInt( $wrapper.data('max') ),
+          min          = parseInt( $wrapper.data('min') );
 
       // clear accordion arrows if multi-instance
       if ( $wrapper.hasClass('ui-accordion') ) {
@@ -854,12 +856,35 @@
 
           if ( $panel.length && !$panel.data( 'opened' ) ) {
 
-            var $fields = $panel.children();
-            var $first  = $fields.first().find(':input').first();
-            var $title  = $header.find('.csf-cloneable-value');
+            var $title = $header.find('.csf-cloneable-value');
+            var inputs = [];
 
-            $first.on('change keyup', function( event ) {
-              $title.text($first.val());
+            $.each(title_by, function( key, title_key ) {
+              inputs.push($panel.find( '[data-depend-id="'+ title_key +'"]' ));
+            });
+
+            $.each(inputs, function( key, $input ) {
+
+              $input.on('change keyup csf.keyup', function() {
+
+                var titles = [];
+
+                $.each(inputs, function( key, $input ) {
+
+                  var input_value = $input.val();
+
+                  if ( input_value ) {
+                    titles.push( input_value );
+                  }
+
+                });
+
+                if ( titles.length ) {
+                  $title.text( titles.join( title_prefix ) );
+                }
+
+              }).trigger('csf.keyup');
+
             });
 
             $panel.csf_reload_script();
@@ -1349,8 +1374,6 @@
           } else {
             thumbnail = attributes.icon;
           }
-
-          console.log(attributes);
 
           if ( $auto_attributes ) {
             $auto_attributes.removeClass('csf--attributes-hidden');
@@ -2504,7 +2527,7 @@
       if ( $form.attr('id') === 'addtag' ) {
 
         var $submit = $form.find('#submit'),
-            $cloned = $this.find('.csf-field').csf_clone();
+            $cloned = $this.children('.csf-field').csf_clone();
 
         $submit.on( 'click', function() {
 
@@ -2590,14 +2613,14 @@
             }
 
             if ( sub_shortcode_value !== '' ) {
-              shortcode += ' ' + sub_shortcode_tag.replace('-', '_') + '="' + sub_shortcode_value.toString() + '"';
+              shortcode += ' ' + sub_shortcode_tag + '="' + sub_shortcode_value.toString() + '"';
             }
 
           });
 
         } else {
 
-          shortcode += ' ' + shortcode_tag.replace('-', '_') + '="' + shortcode_value.toString() + '"';
+          shortcode += ' ' + shortcode_tag + '="' + shortcode_value.toString() + '"';
 
         }
 
@@ -3103,6 +3126,40 @@
   };
 
   //
+  // Helper Check/Uncheck All
+  //
+  $.fn.csf_checkbox_all = function() {
+    return this.each( function() {
+
+      var $this = $(this);
+
+      $this.on('click', function() {
+
+        var $inputs = $this.closest('.csf-field-checkbox').find(':input'),
+            uncheck = false;
+
+        $inputs.each(function() {
+          if ( ! $(this).prop('checked') ) {
+            uncheck = true;
+          }
+        });
+
+        if ( uncheck ) {
+          $inputs.prop('checked', 'checked');
+          $inputs.attr('checked', 'checked');
+        } else {
+          $inputs.prop('checked', '');
+          $inputs.removeAttr('checked');
+        }
+
+        $inputs.first().trigger('change');
+
+      });
+
+    });
+  };
+
+  //
   // Siblings
   //
   $.fn.csf_siblings = function() {
@@ -3235,7 +3292,7 @@
         return;
       }
 
-      $input.on('change keyup', function() {
+      $input.on('change keyup csf.change', function() {
 
         var obj = $this.find(':input').serializeObjectCSF();
         var val = ( !$.isEmptyObject(obj) && obj[unique_id] && obj[unique_id][option_id] ) ? obj[unique_id][option_id] : '';
@@ -3406,6 +3463,7 @@
 
         // Field Checkbox
         $this.children('.csf-field-checkbox').find('.csf-checkbox').csf_checkbox();
+        $this.children('.csf-field-checkbox').find('.csf-checkbox-all').csf_checkbox_all();
 
         // Field Siblings
         $this.children('.csf-field-button_set').find('.csf-siblings').csf_siblings();
